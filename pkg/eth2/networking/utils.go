dde917284be989609596d6e9647299f00cdacf3c
AntiD2ta <mtpotro41@gmail.com>
2022-04-13 22:24:33 -0400

    refac: Remove parseEventData function

--- pkg/eth2/networking/utils.go
+++ pkg/eth2/networking/utils.go
@@ -4,15 +4,6 @@ import (
 	"encoding/json"
 )
 
-func parseEventData(data []byte) (Checkpoint, error) {
-	var c Checkpoint
-	err := json.Unmarshal(data, &c)
-	if err != nil {
-		return c, err
-	}
-	return c, nil
-}
-
 func unmarshalData[J any](data []byte, object J) (J, error) {
 	err := json.Unmarshal(data, &object)
 	if err != nil {
