922c78b4ed3f4ba6b6dd9c904f7efa72c98e4d33
AntiD2ta <mtpotro41@gmail.com>
2022-04-13 22:26:14 -0400

    feat(eth2): Implement getValidatorBalances and Monitor

--- pkg/eth2/eth2.go
+++ pkg/eth2/eth2.go
@@ -2,14 +2,50 @@ package eth2
 
 import (
 	"fmt"
-	"os"
+	"strconv"
+	"time"
 
 	"github.com/NethermindEth/posgonitor/configs"
+	"github.com/NethermindEth/posgonitor/pkg/eth2/db"
+	net "github.com/NethermindEth/posgonitor/pkg/eth2/networking"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func Monitor(handleCfg bool) []chan struct{} {
+// Middleware
+type eth2Monitor struct {
+	repository     db.Repository
+	beaconClient   net.BeaconAPI
+	subscriberOpts net.SubscribeOpts
+}
+
+func DefaultEth2Monitor() (*eth2Monitor, error) {
+	ormdb, err := gorm.Open(sqlite.Open("eth2_monitor.db"), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf(SQLiteCreationError, err)
+	}
+
+	return &eth2Monitor{
+		repository:   &db.SQLiteRepository{DB: ormdb},
+		beaconClient: &net.BeaconClient{RetryDuration: time.Minute},
+		subscriberOpts: net.SubscribeOpts{
+			StreamURL:  net.FinalizedCkptTopic,
+			Subscriber: &net.SSESubscriber{},
+		},
+	}, nil
+}
+
+func NewEth2Monitor(r db.Repository, bc net.BeaconAPI, so net.SubscribeOpts) *eth2Monitor {
+	return &eth2Monitor{
+		repository:     r,
+		beaconClient:   bc,
+		subscriberOpts: so,
+	}
+}
+
+func (e *eth2Monitor) Monitor(handleCfg bool) ([]chan struct{}, error) {
 	if handleCfg {
 		configs.InitConfig()
 	}
@@ -17,35 +53,88 @@ func Monitor(handleCfg bool) []chan struct{} {
 	cfg, err := Init()
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
 
+	// setup beacon nodes endpoints
+	e.subscriberOpts.Endpoints = cfg.Consensus
+	e.beaconClient.SetEndpoints(cfg.Consensus)
+
+	// setup logger
 	configs.InitLogging()
 
 	log.Debugf("Configuration object: %+v", cfg)
 
+	if err = e.repository.Migrate(); err != nil {
+		return nil, fmt.Errorf(MigrationError, err)
+	}
+
 	subDone := make(chan struct{})
-	sub := SubscribeOpts{
-		endpoints:  cfg.Consensus,
-		streamURL:  finalizedCkptTopic,
-		subscriber: &sseSubscriber{},
-	}
-	chkps := subscribe(subDone, sub)
+	chkps := net.Subscribe(subDone, e.subscriberOpts)
 
-	//TODO: Init and setup DB
+	go e.getValidatorBalance(chkps, cfg.Validators)
+	go e.setupAlerts(chkps)
 
-	go getValidatorBalance(chkps)
-	go setupAlerts(chkps)
-
-	return []chan struct{}{subDone}
+	return []chan struct{}{subDone}, nil
 }
 
-func getValidatorBalance(chkps <-chan checkpoint) {
+func (e *eth2Monitor) getValidatorBalance(chkps <-chan net.Checkpoint, validatorsIdxs []string) {
+	logFields := log.Fields{configs.Component: "ETH2 Monitor", "Method": "getValidatorBalance"}
+
 	for c := range chkps {
-		log.WithField(configs.Component, "ETH2").Infof("Got checkpoint: %+v", c)
+		log.WithFields(logFields).Infof("Got Checkpoint: %+v", c)
+
+		// Hardcoding head state for now
+		vbs, err := e.beaconClient.ValidatorBalances("head", validatorsIdxs)
+		if err != nil {
+			log.WithFields(logFields).Errorf(ValidatorBalancesError, err)
+			continue
+		}
+
+		for _, vb := range vbs {
+			log.WithFields(logFields).Debugf("Validator Balance fetched: %+v", vb)
+
+			// Get validator index from response data
+			idx, err := parseUint(vb.Index)
+			if err != nil {
+				log.WithFields(logFields).Errorf(ParseUintError, err)
+				continue
+			}
+
+			// Get validator balance from response data
+			newBalance, err := strconv.ParseUint(vb.Balance, 10, 64)
+			if err != nil {
+				log.WithFields(logFields).Errorf(ParseUintError, err)
+				continue
+			}
+
+			// Get validator from db
+			v, err := e.repository.FirstOrCreate(db.Validator{Idx: idx, Balance: newBalance})
+			if err != nil {
+				log.WithFields(logFields).Errorf(ValidatorNotFoundError, err)
+				continue
+			}
+
+			if newBalance < v.Balance {
+				log.WithFields(logFields).Warnf("Attestation has been missed by %d, count: %d", v.Idx, v.MissedAtts+1)
+				e.repository.Update(db.Validator{
+					Idx:             v.Idx,
+					Balance:         newBalance,
+					MissedAtts:      v.MissedAtts + 1,
+					MissedAttsTotal: v.MissedAttsTotal + 1,
+				})
+			} else {
+				e.repository.Update(db.Validator{
+					Idx:             v.Idx,
+					Balance:         newBalance,
+					MissedAtts:      0,
+					MissedAttsTotal: v.MissedAttsTotal,
+				})
+			}
+		}
 	}
 }
 
-func setupAlerts(<-chan checkpoint) {
+func (e *eth2Monitor) setupAlerts(<-chan net.Checkpoint) {
 
 }
---
+++ pkg/eth2/eth2_test.go
@@ -0,0 +1,289 @@
+package eth2
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/NethermindEth/posgonitor/pkg/eth2/db"
+	net "github.com/NethermindEth/posgonitor/pkg/eth2/networking"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// Mock of BeaconAPI.
+type TestBeaconClient struct {
+	data    [][]net.ValidatorBalance
+	current int
+}
+
+func (tbc *TestBeaconClient) SetEndpoints(endpoints []string) {
+	// do nothing
+}
+
+func (tbc *TestBeaconClient) ValidatorBalances(stateID string, validatorIdxs []string) ([]net.ValidatorBalance, error) {
+	// Simulates an iterator
+	if tbc.current >= len(tbc.data) {
+		return nil, fmt.Errorf("No more data")
+	}
+
+	if tbc.data[tbc.current] == nil {
+		return nil, fmt.Errorf("Intentional error")
+	}
+
+	tbc.current++
+	return tbc.data[tbc.current-1], nil
+}
+
+func fillChannel(data []net.Checkpoint) <-chan net.Checkpoint {
+	ch := make(chan net.Checkpoint, len(data))
+	go func() {
+		for _, d := range data {
+			ch <- d
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func populateDb(r db.Repository, existingData []db.Validator) error {
+	for _, d := range existingData {
+		_, err := r.FirstOrCreate(d)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func newTestBeaconClient(data [][]net.ValidatorBalance) *TestBeaconClient {
+	return &TestBeaconClient{
+		data:    data,
+		current: 0,
+	}
+}
+
+func setup(data [][]net.ValidatorBalance) (*eth2Monitor, error) {
+	ormdb, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("In memory sqlite creation failed. Error '%v'", err)
+	}
+
+	ormdb.AutoMigrate(&db.ValidatorORM{})
+
+	return NewEth2Monitor(&db.SQLiteRepository{DB: ormdb}, newTestBeaconClient(data), net.SubscribeOpts{}), nil
+}
+
+func cleanup(r db.Repository) error {
+	rorm := r.(*db.SQLiteRepository)
+	sqlDB, err := rorm.DB.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Close()
+}
+
+func TestGetValidatorBalance(t *testing.T) {
+	tcs := []struct {
+		name string
+		// input data
+		subscriptionData []net.Checkpoint
+		// data that should be in the db before the test
+		existingData []db.Validator
+		// ordered data returned by the beacon client
+		requestData [][]net.ValidatorBalance
+		// data to validate in test db
+		want []db.Validator
+	}{
+		{
+			name:             "Test case 1, Empty and closed channel, nothing should happen",
+			subscriptionData: []net.Checkpoint{},
+			existingData:     []db.Validator{},
+			requestData:      [][]net.ValidatorBalance{},
+			want:             []db.Validator{},
+		},
+		{
+			name: "Test case 2, One entry in channel, one validator to insert",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "1", Balance: "32000136946"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 0, MissedAttsTotal: 0},
+			},
+		},
+		{
+			name: "Test case 3, One entry in channel, one validator to update, positive balance change",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 0, MissedAttsTotal: 0},
+			},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "1", Balance: "34000136946"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 34000136946, MissedAtts: 0, MissedAttsTotal: 0},
+			},
+		},
+		{
+			name: "Test case 4, One entry in channel, one validator to update, negative balance change",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{
+				{Idx: 1, Balance: 34000136946, MissedAtts: 0, MissedAttsTotal: 0},
+			},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "1", Balance: "32000136946"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 1, MissedAttsTotal: 1},
+			},
+		},
+		{
+			name: "Test case 5, One entry in channel, one validator to update, negative balance change, existing missed atts",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{
+				{Idx: 1, Balance: 34000136946, MissedAtts: 1, MissedAttsTotal: 1},
+			},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "1", Balance: "32000136946"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 2, MissedAttsTotal: 2},
+			},
+		},
+		{
+			name: "Test case 6, One entry in channel, one validator to update, positive balance change, existing missed atts",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 2, MissedAttsTotal: 4},
+			},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "1", Balance: "34000136946"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 34000136946, MissedAtts: 0, MissedAttsTotal: 4},
+			},
+		},
+		{
+			name: "Test case 7, One entry in channel, negative index in request",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 2, MissedAttsTotal: 4},
+			},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "-1", Balance: "34000136946"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 2, MissedAttsTotal: 4},
+			},
+		},
+		{
+			name: "Test case 8, One entry in channel, incorrect balance in request",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 2, MissedAttsTotal: 4},
+			},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "1", Balance: "aaaaa"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 2, MissedAttsTotal: 4},
+			},
+		},
+		{
+			name: "Test case 9, One entry in channel, bad request, no data",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "aaaaa", Balance: "aaaaa"},
+				},
+			},
+			want: []db.Validator{},
+		},
+		{
+			name: "Test case 10, One entry in channel, error from ValidatorBalance, no data",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{},
+			requestData:  [][]net.ValidatorBalance{},
+			want:         []db.Validator{},
+		},
+		{
+			name: "Test case 11, several entries in channel, mixed behavior",
+			subscriptionData: []net.Checkpoint{
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"},
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"},
+				{Block: "0x9a2fefd2fdb57f74993c7780ea5b9030d2897b615b89f808011ca5aebed54eaf", State: "0x600e852a08c1200654ddf11025f1ceacb3c2e74bdd5c630cde0838b2591b69f9", Epoch: "2"}},
+			existingData: []db.Validator{
+				{Idx: 1, Balance: 32000136946, MissedAtts: 6, MissedAttsTotal: 400},
+				{Idx: 2, Balance: 33000136946, MissedAtts: 2, MissedAttsTotal: 30},
+				{Idx: 3, Balance: 35000136946, MissedAtts: 0, MissedAttsTotal: 0},
+			},
+			requestData: [][]net.ValidatorBalance{
+				{
+					{Index: "1", Balance: "33000136946"},
+					{Index: "2", Balance: "31000136946"},
+					{Index: "2", Balance: "30000136946"},
+					{Index: "3", Balance: "36000136946"},
+				},
+			},
+			want: []db.Validator{
+				{Idx: 1, Balance: 33000136946, MissedAtts: 0, MissedAttsTotal: 400},
+				{Idx: 2, Balance: 30000136946, MissedAtts: 4, MissedAttsTotal: 32},
+				{Idx: 3, Balance: 36000136946, MissedAtts: 0, MissedAttsTotal: 0},
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			monitor, err := setup(tc.requestData)
+			if err != nil {
+				t.Fatalf("Setup failed. Error %v", err)
+			}
+
+			err = populateDb(monitor.repository, tc.existingData)
+			if err != nil {
+				t.Fatalf("Populate db failed. Error %v", err)
+			}
+			input := fillChannel(tc.subscriptionData)
+			monitor.getValidatorBalance(input, []string{})
+
+			for _, want := range tc.want {
+				got, err := monitor.repository.Validator(want.Idx)
+				if err != nil {
+					t.Fatalf("Validator %v not found in db", want.Idx)
+				}
+				assert.Equal(t, want, got, "Validator in db with index %v is not equal to the wanted one", want.Idx)
+			}
+
+			if err = cleanup(monitor.repository); err != nil {
+				t.Fatalf("Cleanup failed. Error %v", err)
+			}
+		})
+	}
+}
