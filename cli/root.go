5fa5e73e3e126b3d7b2f4f2bd59123fc8cc0ce49
AntiD2ta <mtpotro41@gmail.com>
2022-05-25 19:45:19 -0400

    feat: 'posmoni' command starts ethereum monitor. Equivalent to 'posmoni ethereum'

--- cli/root.go
+++ cli/root.go
@@ -18,50 +18,26 @@ package cli
 // notest
 import (
 	"os"
-	"os/signal"
-	"time"
 
 	"github.com/spf13/cobra"
 
 	"github.com/NethermindEth/posmoni/configs"
-	"github.com/NethermindEth/posmoni/pkg/eth2"
-	log "github.com/sirupsen/logrus"
 )
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "posmoni [flags]",
+var RootCmd = &cobra.Command{
+	Use:   "posmoni",
 	Short: "A tool to monitor validators in a PoS network of chain, using official HTTP APIs.",
-	Long:  `A tool to monitor validators in a PoS network of chain, using official HTTP APIs.`,
+	Long:  `A tool to monitor validators in a PoS network of chain, using official HTTP APIs. By default runs Ethereum Beacon Chain monitor, equivalent to 'posmoni ethereum'. Run 'posmoni ethereum --help' for more description about command usage.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// listen for SIGINT
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan, os.Interrupt)
-
-		monitor, err := eth2.DefaultEth2Monitor(eth2.ConfigOpts{HandleCfg: false})
-		if err != nil {
-			log.Fatal(err)
-		}
-		doneChans, err := monitor.Monitor()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for range sigChan {
-			log.Info("Received SIGINT, exiting...")
-			for _, done := range doneChans {
-				close(done)
-			}
-			time.Sleep(time.Second)
-			os.Exit(0)
-		}
+		ExecuteEthMonitor()
 	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -70,5 +46,5 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(configs.InitConfig)
 
-	rootCmd.PersistentFlags().StringVar(&configs.CfgFile, "config", "", "config file (default is $HOME/.posmoni.yaml)")
+	RootCmd.PersistentFlags().StringVar(&configs.CfgFile, "config", "", "config file (default is $HOME/.posmoni.yaml)")
 }
