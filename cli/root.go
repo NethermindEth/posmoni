f6b30a421d5759890c32250cef12aef4ac1b4189
Aivaras Ko <aivaras.ko@gmail.com>
2022-10-04 10:11:04 +0300

    Enable prom http endpoint

--- cli/root.go
+++ cli/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,8 +17,11 @@ package cli
 
 // notest
 import (
+	"net/http"
 	"os"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/NethermindEth/posmoni/configs"
@@ -47,4 +50,12 @@ func init() {
 	cobra.OnInitialize(configs.InitConfig)
 
 	RootCmd.PersistentFlags().StringVar(&configs.CfgFile, "config", "", "config file (default is $HOME/.posmoni.yaml)")
+
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		err := http.ListenAndServe(":2112", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
